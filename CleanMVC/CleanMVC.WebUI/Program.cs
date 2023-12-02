0c684f8c1d027bdfda0eef28adacb07ccf0aa812
ElynekerLuciani <elyneker@gmail.com>
2023-12-02 14:15:07 -0300

    atualizando pacotes e ajustando classe program para novo padrão utilizado

--- CleanMVC/CleanMVC.WebUI/Program.cs
+++ CleanMVC/CleanMVC.WebUI/Program.cs
@@ -1,43 +1,64 @@
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddControllersWithViews();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//app.Run();
-
 using CleanMVC.Domain.Account;
+using CleanMVC.Infra.Data.Identity;
 using CleanMVC.InversionOfControl;
-using CleanMVC.WebUI;
-using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddInfrastructure(builder.Configuration);
 
-var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services);
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-startup.Configure(app, app.Environment);
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+//seedUserRoleInitial.SeedRoles();
+//seedUserRoleInitial.SeedUsers();
+SeedUserRoles(app);
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedUserRoles(IApplicationBuilder app)
+{
+    using (var serviceScope = app.ApplicationServices.CreateScope())
+    {
+        var seed = serviceScope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+        seed.SeedUsers();
+        seed.SeedRoles();
+    }
+}
+
+//using CleanMVC.Domain.Account;
+//using CleanMVC.InversionOfControl;
+//using CleanMVC.WebUI;
+//using System.Runtime.CompilerServices;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//var startup = new Startup(builder.Configuration);
+//startup.ConfigureServices(builder.Services);
+
+//var app = builder.Build();
+
+//startup.Configure(app, app.Environment);
+
+//app.Run();
